86b46caee606103d8a9c96f4d3d5e51983a7330f
anys34 <anyeseong34@gmail.com>
2023-09-03 14:53:52 +0900

    ADD :: 서블릿 컨테이너 띄우기

--- helloboot/src/main/java/yeseongspring/helloboot/HellobootApplication.java
+++ helloboot/src/main/java/yeseongspring/helloboot/HellobootApplication.java
@@ -1,13 +1,14 @@
 package yeseongspring.helloboot;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
 
-@SpringBootApplication
 public class HellobootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HellobootApplication.class, args);
+		TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+		WebServer webServer = serverFactory.getWebServer();
+		webServer.start();
 	}
 
 }

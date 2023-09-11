eb2b272bd00ef00b4fe9a1b3b907ccc8e10312c1
anys34 <anyeseong34@gmail.com>
2023-09-12 08:24:18 +0900

    ADD :: 서블릿 등록

--- helloboot/src/main/java/yeseongspring/helloboot/HellobootApplication.java
+++ helloboot/src/main/java/yeseongspring/helloboot/HellobootApplication.java
@@ -2,12 +2,30 @@ package yeseongspring.helloboot;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class HellobootApplication {
 
 	public static void main(String[] args) {
 		TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-		WebServer webServer = serverFactory.getWebServer();
+		WebServer webServer = serverFactory.getWebServer(servletContext -> {
+			servletContext.addServlet("hello", new HttpServlet() {
+				@Override
+				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+					resp.setStatus(200);
+					resp.setHeader("Content-Type", "text/plain");
+					resp.getWriter().println("Hello Servlet");
+				}
+			}).addMapping("/hello");
+
+		});
 		webServer.start();
 	}
 

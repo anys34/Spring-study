33884cfd90ee4440763d48b47d5ee28143ae8271
anys34 <anyeseong34@gmail.com>
2023-09-12 10:11:24 +0900

    ADD :: 서블릿 요청 처리

--- helloboot/src/main/java/yeseongspring/helloboot/HellobootApplication.java
+++ helloboot/src/main/java/yeseongspring/helloboot/HellobootApplication.java
@@ -3,6 +3,9 @@ package yeseongspring.helloboot;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -19,9 +22,11 @@ public class HellobootApplication {
 			servletContext.addServlet("hello", new HttpServlet() {
 				@Override
 				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-					resp.setStatus(200);
-					resp.setHeader("Content-Type", "text/plain");
-					resp.getWriter().println("Hello Servlet");
+					String name = req.getParameter("name");
+
+					resp.setStatus(HttpStatus.OK.value());
+					resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+					resp.getWriter().println("Hello " + name);
 				}
 			}).addMapping("/hello");
 

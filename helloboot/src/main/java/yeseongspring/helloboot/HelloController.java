3ad36fe9f990f6a6ef66eb0a355b45402ba7989a
anys34 <anyeseong34@gmail.com>
2023-09-12 21:32:50 +0900

    ADD :: 의존 오브젝트 추가

--- helloboot/src/main/java/yeseongspring/helloboot/HelloController.java
+++ helloboot/src/main/java/yeseongspring/helloboot/HelloController.java
@@ -1,12 +1,11 @@
 package yeseongspring.helloboot;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import java.util.Objects;
 
-@RestController
 public class HelloController {
-    @GetMapping("/hello")
     public String hello(String name) {
-        return "hello " + name;
+        SimpleHelloService helloService = new SimpleHelloService();
+
+        return helloService.sayHello(Objects.requireNonNull(name));
     }
 }
---
+++ helloboot/src/main/java/yeseongspring/helloboot/SimpleHelloService.java
@@ -0,0 +1,7 @@
+package yeseongspring.helloboot;
+
+public class SimpleHelloService {
+    String sayHello(String name) {
+        return "Hello " + name;
+    }
+}

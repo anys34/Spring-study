6032b62ef1c278fae9fc13ff5ba7b49766ab73f8
anys34 <anyeseong34@gmail.com>
2023-09-12 23:13:38 +0900

    ADD :: 애노테이션 매핑 정보 사용

--- helloboot/src/main/java/yeseongspring/helloboot/HelloController.java
+++ helloboot/src/main/java/yeseongspring/helloboot/HelloController.java
@@ -1,7 +1,13 @@
 package yeseongspring.helloboot;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import java.util.Objects;
 
+@RequestMapping("hello")
 public class HelloController {
     private final HelloService helloService;
 
@@ -9,6 +15,9 @@ public class HelloController {
         this.helloService = helloService;
     }
 
+    @GetMapping
+    @ResponseBody
+    // @RequestMapping(value = "/hello", method = RequestMethod.GET);
     public String hello(String name) {
         return helloService.sayHello(Objects.requireNonNull(name));
     }

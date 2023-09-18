b88c2f461cfe3403a2cbc2ffa2dfca61f0791b48
anys34 <anyeseong34@gmail.com>
2023-09-18 13:59:02 +0900

    ADD :: 테스트 코드를 이용한 테스트

---
+++ helloboot/src/test/java/yeseongspring/helloboot/HelloApiTest.java
@@ -0,0 +1,27 @@
+package yeseongspring.helloboot;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+public class HelloApiTest {
+    @Test
+    void helloApi() {
+        // http localhost:8080/hello?name=Spring
+        TestRestTemplate rest = new TestRestTemplate();
+
+        ResponseEntity<String> res =
+                rest.getForEntity("http://localhost:8080/hello?name={name}", String.class, "Spring");
+
+        // status code 200
+        Assertions.assertThat(res.getStatusCode()).isEqualTo(HttpStatus.OK);
+        // header(content-type) text/plain
+        Assertions.assertThat(res.getHeaders().getFirst(HttpHeaders.CONTENT_TYPE)).startsWith(MediaType.TEXT_PLAIN_VALUE);
+        // body Hello Spring
+        Assertions.assertThat(res.getBody()).isEqualTo("Hello Spring");
+    }
+}

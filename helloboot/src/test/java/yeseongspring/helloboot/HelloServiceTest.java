cc81debf02a4626420c1e450599af01a26a9e182
anys34 <anyeseong34@gmail.com>
2023-09-24 00:07:26 +0900

    ADD :: 메타 애노테이션과 합성 애노테이션

--- helloboot/src/test/java/yeseongspring/helloboot/HelloServiceTest.java
+++ helloboot/src/test/java/yeseongspring/helloboot/HelloServiceTest.java
@@ -3,8 +3,27 @@ package yeseongspring.helloboot;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@UnitTest
+@interface FastUnitTest {
+
+}
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.ANNOTATION_TYPE ,ElementType.METHOD})
+@Test
+@interface UnitTest {
+
+}
+
 public class HelloServiceTest {
-    @Test
+    @UnitTest
     void SimpleHelloService() {
         SimpleHelloService helloService = new SimpleHelloService();
 

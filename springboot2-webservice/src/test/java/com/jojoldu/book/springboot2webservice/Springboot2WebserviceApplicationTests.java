e9fe861893b353d815f7bb6d121cf61f7b0a03db
anys34 <anyeseong34@gmail.com>
2023-09-28 20:47:21 +0000

    ADD :: 프로젝트 환경설정

---
+++ springboot2-webservice/src/test/java/com/jojoldu/book/springboot2webservice/Springboot2WebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jojoldu.book.springboot2webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot2WebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

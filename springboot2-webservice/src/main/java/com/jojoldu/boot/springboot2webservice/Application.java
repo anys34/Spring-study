cdf3d0ec6e23c38cba13c0dc9bbbbd54c64221f9
anys34 <anyeseong34@gmail.com>
2023-09-29 23:52:15 +0900

    ADD :: 3.3 Spring Data JPA 테스트 코드 작성하기

--- springboot2-webservice/src/main/java/com/jojoldu/boot/springboot2webservice/Application.java
+++ springboot2-webservice/src/main/java/com/jojoldu/boot/springboot2webservice/Application.java
@@ -1,4 +1,4 @@
-package com.jojoldu.book.springboot2webservice;
+package com.jojoldu.boot.springboot2webservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ springboot2-webservice/src/main/java/com/jojoldu/boot/springboot2webservice/web/HelloController.java
@@ -0,0 +1,19 @@
+package com.jojoldu.boot.springboot2webservice.web;
+
+import com.jojoldu.boot.springboot2webservice.web.dto.HelloResponseDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount) {
+        return new HelloResponseDto(name, amount);
+    }
+}

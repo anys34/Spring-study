be6197bf5bdae4dec2c510720f2de5947e812ccf
anys34 <anyeseong34@gmail.com>
2023-10-04 00:14:51 +0900

    5.4 어노테이션 기반으로 개선하기

--- springboot2-webservice/src/main/java/com/jojoldu/boot/springboot2webservice/web/IndexController.java
+++ springboot2-webservice/src/main/java/com/jojoldu/boot/springboot2webservice/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.jojoldu.boot.springboot2webservice.web;
 
+import com.jojoldu.boot.springboot2webservice.config.auth.LoginUser;
 import com.jojoldu.boot.springboot2webservice.config.auth.dto.SessionUser;
 import com.jojoldu.boot.springboot2webservice.service.PostsService;
 import com.jojoldu.boot.springboot2webservice.web.dto.PostsResponseDto;
@@ -18,9 +19,8 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         if (user != null) {
             model.addAttribute("userName", user.getName());
